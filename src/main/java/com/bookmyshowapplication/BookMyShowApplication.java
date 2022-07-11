07463ece8c5258cb9819d2bf939f3bcb535b41ca
ak-java543 <88302705+ak-java543@users.noreply.github.com>
2022-07-11 11:13:35 +0530

    update by me

--- src/main/java/com/bookmyshowapplication/BookMyShowApplication.java
+++ src/main/java/com/bookmyshowapplication/BookMyShowApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
        
 @SpringBootApplication
 public class BookMyShowApplication {
+	
+	
 
 	public static void main(String[] args) {
 		
